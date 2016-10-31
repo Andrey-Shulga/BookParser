95c43e759d39e9052aaa8e8f01037de64270dd9b
Andrey Shulga <solyariz@gmail.com>
2016-11-01 01:56:50 +0600

    Added log for catch IO exception

--- src/main/java/com/epam/as/bookparser/util/PropertyManager.java
+++ src/main/java/com/epam/as/bookparser/util/PropertyManager.java
@@ -1,5 +1,8 @@
 package com.epam.as.bookparser.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -10,13 +13,14 @@ import java.util.Properties;
 public class PropertyManager {
 
     private Properties properties;
+    Logger errorLogger = LoggerFactory.getLogger("errorLogger");
 
     public PropertyManager(String propertyFileName) {
         properties = new Properties();
         try (InputStream in = PropertyManager.class.getClassLoader().getResourceAsStream(propertyFileName)) {
             properties.load(in);
         } catch (IOException e) {
-            e.printStackTrace();
+            errorLogger.error("Couldn't open file: {}", propertyFileName);
         }
     }
 
